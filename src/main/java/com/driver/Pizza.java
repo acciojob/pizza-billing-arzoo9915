f0deae1b6962eb0e95e6a1c12fff88de0f421c5e
unknown <arzoogoyal321@gmail.com>
2023-02-07 13:15:22 +0530

    sol2

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -34,6 +34,7 @@ public class Pizza {
             this.toppings=120;
         }
         this.cheese=80;
+        this.bill+="Base Price Of The Pizza: "+this.price+"\n";
         // your code goes here
     }
 
